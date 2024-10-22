0f703d8224265dca92383536e6b521a6306e6da4
Rojer Hein <164485492+HeinRojer@users.noreply.github.com>
2024-10-22 20:55:52 +0530

    Add files via upload

---
+++ GameTagGenerator__main.py
@@ -0,0 +1,7 @@
+# day 1 project: concating string
+#this program creates you a simple gametag
+print("Welcome To Game Tag Generator")
+name1= input("Enter your first name\n")
+name2= input("Enter your second name\n")
+num= input('enter your favourite number\n')
+print(f"Your game tag could be :{name2}{name1}{num} ")
